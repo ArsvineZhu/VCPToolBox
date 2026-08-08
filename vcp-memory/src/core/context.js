be3f3e047acbd315026f334549dd2c6b646a2e7f
Arsvine Zhu <arsvinezhu@gmail.com>
2026-08-08 11:30:43 -0600

    feat(vcp-memory): implement memo stages (epa-projector, residual-pyramid, tag-expander, vector-reshaper)

--- vcp-memory/src/core/context.js
+++ vcp-memory/src/core/context.js
@@ -10,14 +10,16 @@ class PipelineContext {
    * @param {import('../interfaces/embedding-provider')} [opts.embeddingProvider]
    * @param {import('../interfaces/vector-store')} [opts.vectorStore]
    * @param {import('../interfaces/metadata-store')} [opts.metadataStore]
-   * @param {object} [opts.vexusIndex] - Raw Rust N-API handle for algorithm layer
-   */
-  constructor({ config, embeddingProvider, vectorStore, metadataStore, vexusIndex }) {
+* @param {object} [opts.vexusIndex] - Raw Rust N-API handle for algorithm layer
+    * @param {import('../algorithms/epa').EPA} [opts.epa] - Pre-built EPA basis for the memo pipeline
+    */
+  constructor({ config, embeddingProvider, vectorStore, metadataStore, vexusIndex, epa }) {
     this.config = config;
     this.embeddingProvider = embeddingProvider;
     this.vectorStore = vectorStore;
     this.metadataStore = metadataStore;
     this.vexusIndex = vexusIndex;
+    this.epa = epa;
   }
 }
 
